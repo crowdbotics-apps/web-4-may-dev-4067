b0e8d4fa50b3aa208983d6b6a85ee54437d81ddf
Crowdbotics <team@crowdbotics.com>
2020-05-04 13:23:26 +0000

    Update models

--- fgnbfhvc/models.py
+++ fgnbfhvc/models.py
@@ -7,6 +7,16 @@ class Tgjf(models.Model):
     cfgsf = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="tgjf_cfgsf",
     )
+    sdasf = models.BigIntegerField(null=True, blank=True,)
+    safasdf = models.BigIntegerField(null=True, blank=True,)
+    erett = models.BigIntegerField(null=True, blank=True,)
+    tyjyuj = models.BigIntegerField(null=True, blank=True,)
+    ikiol = models.BigIntegerField(null=True, blank=True,)
+    qwsqwswqq = models.BigIntegerField(null=True, blank=True,)
+    wdwdwe = models.BigIntegerField(null=True, blank=True,)
+    dewdwee = models.BigIntegerField(null=True, blank=True,)
+    efrfrfrf = models.BigIntegerField(null=True, blank=True,)
+    erfrgth = models.BigIntegerField(null=True, blank=True,)
 
 
 # Create your models here.
--- homefgh/migrations/0001_initial.py
+++ homefgh/migrations/0001_initial.py
@@ -7,15 +7,22 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Ddfgssfg',
+            name="Ddfgssfg",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('rgs', models.BigIntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("rgs", models.BigIntegerField()),
             ],
         ),
     ]
